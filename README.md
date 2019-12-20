e0029423ef2b575915a9cd9478c99ae81efb23ad
renzheng0403 <191162792@qq.com>
2019-12-21 02:34:33 +0800

    完成编译与测试

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# ndg_hello
-nginx hello module
+# ndg_hello 模块设计
+- 模块名：ngx_http_ndg_hello_module
+- 配置指令：ndg_hello on | off，开关模块功能，只能在 location 里配置
+- 不直接处理 HTTP 请求，只在 URL 重写阶段里执行
+- 根据配置指令的 on|off 决定输出字符串的内容
+- 编写 config 脚本，用 `--add-module` 静态链接选项集成进Nginx
+# 编译
+假设该模块位于 /home/test/ndg_hello 文件夹下：
+```
+./configure --add-module=/home/test/ndg_hello
+make
+sudo make install
+```
+# 测试验证
+配置参数
+```
+master_process off;
+daemon off;
+
+location /hello {
+    ndg_hello on;
+}
+```
+保存重启 nginx 后，使用 curl 访问
+```
+curl -v 'http://localhost/hello'
+```
+可在控制台或日志中查找到相应字符
--- ngx_http_ndg_hello_module.c
+++ ngx_http_ndg_hello_module.c
@@ -1,5 +1,9 @@
 #include <ngx_http.h>
 
+static void *ngx_http_ndg_hello_create_loc_conf(ngx_conf_t* cf);
+static ngx_int_t ngx_http_ndg_hello_handler(ngx_http_request_t *r);
+static ngx_int_t ngx_http_ndg_hello_init(ngx_conf_t* cf);
+
 /**
  * 配置的数据结构
  */
@@ -13,16 +17,49 @@ typedef struct {
 static ngx_command_t ngx_http_ndg_hello_cmds[] =
 {
     {
-        ngx_string("ndg_hello"), //指令的名字
-        NGX_HTTP_LOC_CONF|NGX_CONF_FLAG, //指令的作用域和类型
-        ngx_conf_set_flag_slot,             //解析函数指针
-        NGX_HTTP_LOC_CONF_OFFSET,           //数据的存储位置
+        ngx_string("ndg_hello"),                                        //指令的名字
+        NGX_HTTP_LOC_CONF|NGX_CONF_FLAG,    //指令的作用域和类型
+        ngx_conf_set_flag_slot,                                         //解析函数指针
+        NGX_HTTP_LOC_CONF_OFFSET,                       //数据的存储位置
         offsetof(
-            ngx_http_ndg_hello_loc_conf_t, enable
-        ),
+            ngx_http_ndg_hello_loc_conf_t, enable),
         NULL
     },
-    ngx_null_command                        //空对象，结束数组
+    ngx_null_command                                                    //空对象，结束数组
+};
+
+/**
+ * 集成配置函数
+ */
+static ngx_http_module_t ngx_http_ndg_hello_module_ctx = 
+{
+    NULL,                                                                         //解析配置文件前被调用
+    ngx_http_ndg_hello_init,                                //解析配置文件后被调用
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    ngx_http_ndg_hello_create_loc_conf,     //创建location域的配置结构
+    NULL,
+};
+
+/**
+ * 集成配置指令
+ */
+ngx_module_t ngx_http_ndg_hello_module =
+{
+    NGX_MODULE_V1,                                          //标准的填充宏
+    &ngx_http_ndg_hello_module_ctx,         //配置功能函数
+    ngx_http_ndg_hello_cmds,                        //配置指令数组
+    NGX_HTTP_MODULE,                                    //http 模块必须的tag
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NGX_MODULE_V1_PADDING
 };
 
 /**
@@ -35,8 +72,7 @@ ngx_http_ndg_hello_create_loc_conf(ngx_conf_t* cf)  //创建配置数据结构
 
     conf = ngx_pcalloc(cf->pool,                    //由内存池分配内存
             sizeof(ngx_http_ndg_hello_loc_conf_t));
-    if (conf == NULL)
-    {
+    if (conf == NULL) {
         return NULL;
     }
     conf->enable = NGX_CONF_UNSET;
@@ -47,12 +83,11 @@ ngx_http_ndg_hello_create_loc_conf(ngx_conf_t* cf)  //创建配置数据结构
  * 处理函数
  */
 static ngx_int_t
-ngx_http_ndg_hello_handler(ngx_http_request_t *r)       //处理函数
+ngx_http_ndg_hello_handler(ngx_http_request_t *r)
 {
-    ngx_http_ndg_hello_loc_conf_t* lcf;                 //配置结构体指针
-    lcf = ngx_http_get_module_loc_conf(                 //获取配置数据
-        r, ngx_http_ndg_hello_module
-    );
+    ngx_http_ndg_hello_loc_conf_t* lcf;                           //配置结构体指针
+    lcf = ngx_http_get_module_loc_conf(                         //获取配置数据
+        r, ngx_http_ndg_hello_module);
     if (lcf->enable) {
         printf("hello nginx\n");
         ngx_log_error(NGX_LOG_ERR, r->connection->log,
@@ -68,52 +103,16 @@ ngx_http_ndg_hello_handler(ngx_http_request_t *r)       //处理函数
  * 注册函数
  */
 static ngx_int_t
-ngx_http_ndg_hello_init(ngx_conf_t* cf)                 //注册函数
+ngx_http_ndg_hello_init(ngx_conf_t* cf)
 {
     ngx_http_handler_pt *h;                             //处理函数指针
     ngx_http_core_main_conf_t *cmcf;                    //http 核心配置
     cmcf = ngx_http_conf_get_module_main_conf(
-        cf, ngx_http_core_module
-    );
+        cf, ngx_http_core_module);
 
     h = ngx_array_push(
-        &cmcf->phases[NGX_HTTP_REWRITE_PHASE].handlers
-    );
+        &cmcf->phases[NGX_HTTP_REWRITE_PHASE].handlers);
     *h = ngx_http_ndg_hello_handler;
 
     return NGX_OK;
 }
-
-/**
- * 集成配置函数
- */
-static ngx_http_module_t ngx_http_ndg_hello_module_ctx = 
-{
-    NULL,
-    ngx_http_ndg_hello_init,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    ngx_http_ndg_hello_create_loc_conf,
-    NULL,
-};
-
-/**
- * 集成配置指令
- */
-ngx_module_t ngx_http_ndg_hello_module =
-{
-    NGX_MODULE_V1,
-    &ngx_http_ndg_hello_module_ctx,
-    ngx_http_ndg_hello_cmds,
-    NGX_HTTP_MODULE,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NGX__MODULE_V1_PADDING
-};
