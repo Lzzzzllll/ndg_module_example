69240c314a49dfde46b3bc906a0e92f131de1ce2
renzheng0403 <191162792@qq.com>
2019-12-22 23:27:31 +0800

    添加ndg_echo模块

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
-# ndg_hello 模块设计
+# nginx 模块开发实例
+- ndg_hello
+- ndg_echo
+# ndg_hello 模块
+## ndg_hello 模块设计
 - 模块名：ngx_http_ndg_hello_module
 - 配置指令：ndg_hello on | off，开关模块功能，只能在 location 里配置
 - 不直接处理 HTTP 请求，只在 URL 重写阶段里执行
 - 根据配置指令的 on|off 决定输出字符串的内容
 - 编写 config 脚本，用 `--add-module` 静态链接选项集成进Nginx
-# 编译
-假设该模块位于 /home/test/ndg_hello 文件夹下：
+## 编译
+假设该模块位于 /home/test/ndg_module_example/ndg_hello 文件夹下：
 ```
-./configure --add-module=/home/test/ndg_hello
+./configure \
+--add-module=/home/test/ndg_module_example/ndg_hello
 make
 sudo make install
 ```
-# 测试验证
+## 测试验证
 配置参数
 ```
 master_process off;
@@ -26,3 +31,13 @@ location /hello {
 curl -v 'http://localhost/hello'
 ```
 可在控制台或日志中查找到相应字符
+# ndg_echo 模块
+## ndg_echo 模块设计
+- 模块名：ngx_http_ndg_echo_module
+- 配置指令：ndg_echo，接受一个参数
+- 是一个内容处理模块
+- 只接受 GET 请求方法
+- 使用 content handler 的方式注册处理函数
+- 功能：
+  - 向客户端输出一个指定的字符串信息（由 ndg_echo 指定）
+  - uri 里的参数信息也一并输出
---
+++ ndg_echo/ngx_http_ndg_echo_module.c
@@ -0,0 +1,98 @@
+#include <ngx_http.h>
+
+static ngx_int_t ngx_http_ndg_echo();
+
+typedef struct {
+    ngx_str_t msg;      //存储配置文件里的字符串
+} ngx_http_ndg_echo_loc_conf_t;     //配置结构体
+
+/**
+ * 模块的指令数组定义
+ */
+static ngx_command_t ngx_http_ndg_echo_cmds[] =
+{
+    {
+        ngx_string("ndg_echo"),     //指令的名字
+        NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,       //指令的作用域和类型
+        ngx_http_ndg_echo,      //解析函数
+        NGX_HTTP_LOC_CONF_OFFSET,       //只能在 location 里出现
+        offsetof(       //解析使用的字段偏移量
+            ngx_http_ndg_echo_loc_conf_t, msg),
+        NULL
+    },
+    ngx_null_command
+};
+
+static ngx_http_module_t ngx_http_ndg_echo_module_ctx =
+{
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    ngx_http_ndg_echo_create_loc_conf,
+    NULL,
+};
+
+ngx_module_t ngx_http_ndg_echo_module =
+{
+    NGX_MODULE_V1,
+    &ngx_http_ndg_echo_module_ctx,
+    ngx_http_ndg_echo_cmds,
+    NGX_HTTP_MODULE,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NGX_MODULE_V1_PADDING
+};
+
+static void *
+ngx_http_ndg_echo_create_loc_conf(ngx_conf_t* cf)
+{
+    ngx_http_ndg_echo_loc_conf_t* conf;  //配置结构体指针
+
+    conf = ngx_pcalloc(cf->pool,        //内存池创建结构体对象
+            sizeof(ngx_http_ndg_echo_loc_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+    return conf;
+}
+
+static ngx_int_t
+ngx_http_ndg_echo_handler(ngx_http_request_t *r)
+{
+    size_t len;
+    ngx_int_t rc;
+    ngx_http_ndg_echo_loc_conf_t *lcf;
+
+    if (!(r->method & NGX_HTTP_GET)) {  //检查请求方法
+        return NGX_HTTP_NOT_ALLOWED;    //要求必须是 GET
+    }
+
+    rc = ngx_http_discard_request_body(r);
+    lcf = ngx_http_get_module_loc_conf(r, ngx_http_ndg_echo_module);
+
+    len = lcf->msg.len;
+}
+
+static ngx_int_t
+ngx_http_ndg_echo(ngx_conf_t* cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_core_loc_conf_t* clcf;
+    char* rc = ngx_conf_set_str_slot(cf, cmd, conf);
+    if (rc != NGX_CONF_OK) {
+        return rc;
+    }
+
+    clcf = ngx_http_conf_get_module_loc_conf(
+            cf, ngx_http_core_module);
+    clcf->handler = ngx_http_ndg_echo_handler;
+
+    return NGX_CONF_OK;
+}
